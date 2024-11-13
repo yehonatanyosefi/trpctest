70aff2601cb41a847f8e09540daf63b340005aef
Yonatan Lavy <yehonatanmind@gmail.com>
2024-11-13 15:43:07 +0200

    moved to the trpc 11 set up i used before

--- client/query-client.ts
+++ client/query-client.ts
@@ -2,16 +2,24 @@ import { defaultShouldDehydrateQuery, QueryClient } from '@tanstack/react-query'
 import SuperJSON from 'superjson'
 
 export const createQueryClient = () =>
-    new QueryClient({
-        defaultOptions: {
-            queries: {
-                // With SSR, we usually want to set some default staleTime
-                // above 0 to avoid refetching immediately on the client
-                staleTime: 0,
-                cacheTime: 0,
-                refetchOnMount: true,
-                refetchOnWindowFocus: true,
-                refetchOnReconnect: true,
-            },
-        },
-    })
+	new QueryClient({
+		defaultOptions: {
+			queries: {
+				// With SSR, we usually want to set some default staleTime
+				// above 0 to avoid refetching immediately on the client
+				staleTime: 0,
+				gcTime: 0,
+				refetchOnMount: true,
+				refetchOnWindowFocus: true,
+				refetchOnReconnect: true,
+			},
+			dehydrate: {
+				serializeData: SuperJSON.serialize,
+				shouldDehydrateQuery: (query) =>
+					defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
+			},
+			hydrate: {
+				deserializeData: SuperJSON.deserialize,
+			},
+		},
+	})
